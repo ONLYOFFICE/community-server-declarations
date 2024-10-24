2ed38f6434130a9f7d7c730e1ec7d0097f023778
Andrei Panov <andrew.panov@onlyoffice.com>
2024-10-24 13:12:05 +0300

    map api controller names

--- src/DocumentationUtility.Portals/Models/PortalApiController.cs
+++ src/DocumentationUtility.Portals/Models/PortalApiController.cs
@@ -1,7 +1,7 @@
 ﻿using ASC.Api.Attributes;
 using DocumentationUtility.Shared.Models;
-using DocumentationUtility.Shared.Statistics;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Xml.Linq;
@@ -24,11 +24,35 @@ namespace DocumentationUtility.Portals.Models
         protected override void ParseReflection()
         {
             Path = Name;
+            if (NameMap.TryGetValue(Name, out var mapped))
+            {
+                Name = mapped;
+            }
         }
 
         protected override bool HandleElement(XElement element)
         {
             return base.HandleElement(element);
         }
+
+        private static Dictionary<string, string> NameMap = new Dictionary<string, string>()
+        {
+            { "authentication", "Authentication" },
+            { "calendar", "Calendar" },
+            { "capabilities", "Capabilities" },
+            { "community", "Community" },
+            { "crm", "CRM" },
+            { "feed", "Feed" },
+            { "files", "Files" },
+            { "group", "Group" },
+            { "mail", "Mail" },
+            { "mailserver", "Mail Server" },
+            { "migration", "Migration" },
+            { "people", "People" },
+            { "portal", "Portal" },
+            { "project", "Project" },
+            { "security", "Security" },
+            { "settings", "Settings" },
+        };
     }
 }
